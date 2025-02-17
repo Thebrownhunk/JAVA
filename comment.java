7e3e93aa1233cfe5fbb20c6ca39499aba8040a7c
Ankit kumar sahu <ankit3012sahu@gmail.com>
2025-02-17 23:09:29 +0530

    comment in java

---
+++ comment.java
@@ -0,0 +1,10 @@
+public class comment {
+    public static void main(String[] args) {
+        //System.out.println("Welcome to java");  //single line comment
+
+        /*  System.out.println("I am a good boy");  //multi line comment
+          System.out.println("I am a bad boy"); */
+
+        System.out.println("I am Ankit");
+    } 
+}
