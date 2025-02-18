21eba71174854edded6da5cf7fde90bac699c80a
Ankit kumar sahu <ankit3012sahu@gmail.com>
2025-02-18 22:35:00 +0530

    variable in java

---
+++ variable.java
@@ -0,0 +1,22 @@
+public class variable {
+    public static void main(String[] args){
+
+        //VARIABLE
+        //syntax:- dataType variableName = Value;
+
+        //Rule's of nameing a variable:-
+        //Name's can contain letter,digit,underscore,dollarsign.
+        //Name's must begin with letter.
+        //Name's should start with a lower case letter and can not contain white sapce.
+        //Name's can also begin with $ and _ .
+        //Name's are case sensitive.
+        //Reserved word can not be used as name.
+
+        int myNum = 10;
+        System.out.println(myNum);
+
+        //Declar Multiple Variable's:-
+        int x = 10 , y = 20 , z = 30 ;
+        System.out.println(x+y+z);
+    }
+}
