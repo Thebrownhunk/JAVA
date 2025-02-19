ad34c6a2020698ddea369217206192da517deed6
Ankit kumar sahu <ankit3012sahu@gmail.com>
2025-02-19 14:41:58 +0530

    Primitive data types

---
+++ datatype.java
@@ -0,0 +1,37 @@
+public class datatype {
+    public static void main(String[] args){
+
+        //Datatype
+        //Data types are divided into two groups:
+        //Primitive data types - includes byte, short, int, long, float, double, boolean and char
+        //Non-primitive data types - such as String, Arrays and Classes.
+
+        //Primitive data types
+        int myintNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;    // Floating point number
+        char myLetter = 'D';         // Character
+        boolean myBool = true;       // Boolean
+        String myText = "Hello";     // String
+        byte myByteNum = 100;        // Byte
+        short myShortNum = 5000;
+        long myLongNum = 15000000000L;
+        System.out.println(myintNum);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+        System.out.println(myByteNum);
+        System.out.println(myShortNum);
+        System.out.println(myLongNum );
+
+        // Data Type	Description
+        // byte	Stores whole numbers from -128 to 127
+        // short	Stores whole numbers from -32,768 to 32,767
+        // int	Stores whole numbers from -2,147,483,648 to 2,147,483,647
+        // long	Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        // float	Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+        // double	Stores fractional numbers. Sufficient for storing 15 to 16 decimal digits
+        // boolean	Stores true or false values
+        // char	Stores a single character/letter or ASCII values
+    }
+}
