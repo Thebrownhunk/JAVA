672800aef1af093dda28f4e094070e9cd460637b
Ankit kumar sahu <ankit3012sahu@gmail.com>
2025-02-21 00:34:27 +0530

    Taking user input in java

---
+++ userInput.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class userInput {
+    public static void main(String[] args) {
+        
+        System.out.println("Taking input from user");
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("enter a number:-");
+        int number = sc.nextInt();  // Read a number
+        System.out.println("You entered number: " + number);
+        
+        // Consume the newline character left by nextInt
+        sc.nextLine();  // This will prevent problems when reading the next line
+        
+        System.out.print("enter a string:-");
+        String text = sc.nextLine();  // Read a string
+        System.out.println("You entered string: " + text);
+
+        sc.close();
+
+        // System.out.print("enter number 1:-");
+        // int a = sc.nextInt();
+        // System.out.print("enter number 2:-");
+        // int b = sc.nextInt();
+        // int sum = a+b;
+        // System.out.println("the sum of these number is :"+sum);
+    }
+}
