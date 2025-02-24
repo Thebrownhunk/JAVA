f0b771c0d7de9b4cf015f1bda2b75a5aa9b47f9c
Ankit kumar sahu <ankit3012sahu@gmail.com>
2025-02-24 22:51:02 +0530

    string in java

---
+++ stringggg.java
@@ -0,0 +1,14 @@
+import java.lang.String;
+public class stringggg {
+    public static void main(String[] args) {
+        
+        //Strings
+        //A String variable contains a collection of characters surrounded by double quotes
+        //Strings are used for storing text.
+        //A String in Java is actually an object, which contain methods that can perform certain operations on strings.
+
+        //String name = new String("ankit");  
+        String name = "ankit";
+        System.out.println("My name is " + name);
+    }
+}
