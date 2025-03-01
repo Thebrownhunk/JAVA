ac4fdb179926657d0fcf90953d5537d2f600076a
Ankit kumar sahu <ankit3012sahu@gmail.com>
2025-03-01 23:34:32 +0530

    array in java

--- array.java
+++ array.java
@@ -1,46 +1,61 @@
 public class array {
     public static void main(String[] args) {
+
         //Arrays
+
         //An array is a collection of similar type of elements which has contiguous memory location.
         //Java array is an object which contains elements of a similar data type.
         //It is a data structure where we store similar elements.
         //We can store only a fixed set of elements in a Java array.
         //Array in Java is index-based, the first element of the array is stored at the 0th index, 2nd element is stored on 1st index and so on.
         //The length of the array is established when the array is created and cannot be changed.
+
         //The syntax to declare an array is:
         //datatype[] arrayname;
-        //or
+              //or
         //datatype arrayname[];
+
         //The syntax to initialize an array is:
         //datatype[] arrayname = new datatype[size];
-        //or
+              //or
         //datatype arrayname[] = new datatype[size];
+
         //The syntax to initialize an array with values is:
         //datatype[] arrayname = {value1, value2, value3, value4, ...};
-        //or
+              //or
         //datatype arrayname[] = {value1, value2, value3, value4, ...};
+
         //The syntax to access an array element is:
         //arrayname[index];
+
         //The syntax to change an array element is:
         //arrayname[index] = newvalue;
+
         //The syntax to get the length of an array is:
         //arrayname.length;
+
         //The syntax to iterate through an array is:
         //for (int i = 0; i < arrayname.length; i++) {
         //    System.out.println(arrayname[i]);
         //}
+
         //The syntax to iterate through an array using the for-each loop is:
         //for (datatype element : arrayname) {
         //    System.out.println(element);
         //}
+
         //The syntax to sort an array is:
         //Arrays.sort(arrayname);
+
         //The syntax to search an element in an array is:
         //Arrays.binarySearch(arrayname, element);
+
         //The syntax to copy an array is:
         //Arrays.copyOf(arrayname, newlength);
+
         //The syntax to compare two arrays is:
         //Arrays.equals(arrayname1, arrayname2);
+        
         //The syntax to fill an array is:
         //Arrays.fill(arrayname, value);
         
