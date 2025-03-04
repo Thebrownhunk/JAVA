564a37c5de024431e17cfef26589ea49e6d5e4eb
Ankit kumar sahu <ankit3012sahu@gmail.com>
2025-03-05 00:40:11 +0530

    array in java

--- array.java
+++ array.java
@@ -4,7 +4,7 @@ public class array {
         //Arrays
 
         //An array is a collection of similar type of elements which has contiguous memory location.
-        
+
         //Java array is an object which contains elements of a similar data type.
         //It is a data structure where we store similar elements.
         //We can store only a fixed set of elements in a Java array.
@@ -13,40 +13,88 @@ public class array {
 
         //The syntax to declare an array is:
 
-        //datatype[] arrayname;
+        //Syntax-1 :- datatype[] arrayname;
+        //Ex :-
+             int[] marks1; //declaration
               //or
-        //datatype arrayname[];
+        //Syntax-2 :- datatype arrayname[];
+        //Ex :-
+             int marks2[]; //declaration
+
 
         //The syntax to initialize an array is:
 
-        //datatype[] arrayname = new datatype[size];
+        //Syntax-1 :- datatype[] arrayname = new datatype[size];
+        //Ex :-
+             int[] marks3 = new int[5]; //declaration + memory allocation
+             marks3[0] = 100;             // initialize
+             marks3[1] = 90;              // initialize
+             marks3[2] = 80;              // initialize
+             marks3[3] = 70;              // initialize
+             marks3[4] = 60;              // initialize
+             System.out.println(marks3[1]);
               //or
-        //datatype arrayname[] = new datatype[size];
+        //Syntax-2 :- datatype arrayname[] = new datatype[size];
+        //Ex :-
+             int marks4[] = new int[5]; // declaration + memory allocation
+             marks4[0] = 100;             // initialize
+             marks4[1] = 90;              // initialize
+             marks4[2] = 80;              // initialize
+             marks4[3] = 70;              // initialize
+             marks4[4] = 60;              // initialize
+             System.out.println(marks4[1]);
+
 
         //The syntax to initialize an array with values is:
         
         //datatype[] arrayname = {value1, value2, value3, value4, ...};
+        //Ex :-
+             int[] marks5 = {20,30,40,50,60}; // declaration + memory allocation + initialize
+             System.out.println(marks5[1]);
+        
               //or
         //datatype arrayname[] = {value1, value2, value3, value4, ...};
+        //Ex :-
+             int marks6[] = {20,30,40,50,60}; // declaration + memory allocation + initialize
+             System.out.println(marks6[1]);
 
         //The syntax to access an array element is:
         //arrayname[index];
+        //Ex :- 
+              int math = marks4[0];
+              System.out.println(math);
 
         //The syntax to change an array element is:
         //arrayname[index] = newvalue;
+        //Ex :- 
+             marks4[0] = 98;
+             System.out.println(marks4[0]);
+
 
         //The syntax to get the length of an array is:
         //arrayname.length;
+        //Ex :- 
+             System.out.println(marks6.length);
 
         //The syntax to iterate through an array is:
-        //for (int i = 0; i < arrayname.length; i++) {
-        //    System.out.println(arrayname[i]);
-        //}
+        // syntax :- for (int i = 0; i < arrayname.length; i++) {
+        //                  System.out.println(arrayname[i]);
+        //            }
+        //Ex :-
+               String[] cars1 = {"Volvo", "BMW", "Ford", "Mazda"};
+                for (int i = 0; i < cars1.length; i++) {
+                System.out.println(cars1[i]);
+                }
 
         //The syntax to iterate through an array using the for-each loop is:
-        //for (datatype element : arrayname) {
-        //    System.out.println(element);
-        //}
+        // syntax :- for (datatype element : arrayname) {
+        //                System.out.println(element);
+        //           }
+        //Ex :-
+                String[] cars2 = {"Volvo", "BMW", "Ford", "Mazda"};
+                for (String i : cars2) {
+                System.out.println(i);
+                }
 
         //The syntax to sort an array is:
         //Arrays.sort(arrayname);
